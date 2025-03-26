909cad70e51142fb0d142d32e60b3c7b08588a6c
YARO0926 <cjwony0926@gmail.com>
2025-03-26 14:25:29 +0900

    3.26 3주차 과제제

--- 파이썬 과제/진수변환기 과제 202511290 정지원.py
+++ 파이썬 과제/진수변환기 과제 202511290 정지원.py
@@ -1,2 +1,13 @@
-base=input("입력 진수 결정")
+base=int(input("입력 진수 결정"))
 num=input("값 입력")
+
+num=int(num, base)
+
+hex=hex(num)
+oct=oct(num)
+bin=bin(num)
+
+print("16진수==>"+hex)
+print("8진수==>"+oct)
+print("2진수==>"+bin)
+print("2진수==>"+str(num))
