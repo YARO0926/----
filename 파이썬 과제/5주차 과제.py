cb0bfcdc41172d5183f564ed635e2b2fdf052ad6
YARO0926 <cjwony0926@gmail.com>
2025-04-09 11:59:24 +0900

    2025.04.09 5주차차

---
+++ 파이썬 과제/5주차 과제.py
@@ -0,0 +1,8 @@
+for i in range(2,10):
+    print("#  {}단  # ".format(i),end="")
+
+
+for num in range(1,10):
+    print()
+    for dan in range(2,10):
+        print(f"{dan}x  {num}= {dan*num:>2} ",end="")
--- 파이썬 과제/turtle 연습.py
+++ 파이썬 과제/turtle 연습.py
@@ -10,10 +10,19 @@ t.penup()
 t.goto(0,-sheight / 2)
 t.pendown()
 t.speed(10)
-for radius in range(1,250)
-if radius % 6 ==0:
-    t.pencolor('red')
-elif radius % 5 ==0:
-    t.pencolor('orange')
-elif radius % 4 ==0:
-    
+for radius in range(1,250):
+    if radius % 6 ==0:
+        t.pencolor(red)
+    elif radius % 5 ==0:
+        t.pencolor(orange)
+    elif radius % 4 ==0:
+        t.pencolor(yellow)
+    elif radius % 3 ==0:
+        t.pencolor(green)
+    elif radius % 2 ==0:
+        t.pencolor(navyblue)
+    else :
+        t.pencolor(purple)
+
+        t.circle(radius)
+t.done()
